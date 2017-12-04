a0ef1001c28672f34bd7f7169920c3df4fed6be4
J <gerardo.di.francesco1@gmail.com>
2017-12-04 18:10:33 +0100

    Update conta-il-tempo.js
    Ciao Alberto,
    Ti sembra corretto l'esercizio?
    un saluto
    g

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,3 +6,11 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+var sec = 94089999890;
+var min = Math.floor (sec/60);
+var ore = Math.floor (min/60);
+var giorni = Math.floor (ore/24);
+var oreleft = ore-giorni*24;
+var minleft = min-oreleft*60-giorni*24*60;
+var secleft = sec-minleft*60-oreleft*60*60-giorni*24*60*60;
+console.log(`Sono passati ${giorni} giorni, ${oreleft} ore, ${minleft} minuti e ${secleft} secondi.`);
